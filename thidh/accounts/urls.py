ef585d92e9b29517184fa62b6a9c23ae9b864e39
tUNg131 <59093329+tUNg131@users.noreply.github.com>
2022-08-21 15:02:29 +0100

    1st proto of test draft

--- thidh/accounts/urls.py
+++ thidh/accounts/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "accounts"
 urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
-    path("logout/", views.LogoutView.as_view()),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
     path("password_change/", views.PasswordChangeView.as_view()),
     path("password_change/done/", views.PasswordChangeDoneView.as_view()),
     path("password_reset/", views.PasswordResetView.as_view(), name="password_reset"),
---
+++ tmp/draft.py
@@ -0,0 +1,17 @@
+# context = {
+#     "toan": {
+#         "pastpapers": [{
+#             "verbose_name": "THPT Quốc Gia 202x",
+#             "is_submitted": False,
+#             "name": "", # URL name
+#             "correct_question_count": 13
+#         }],
+#         "mockpapers": [{
+#             "verbose_name": "Đề thi thử",
+#             "is_submitted": None,
+#             "name": "", # URL name
+#             "correct_question_count": None,
+#         }]
+#     },
+#     "ly": {},
+# }
